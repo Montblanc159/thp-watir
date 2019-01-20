634b747881d5d51ea1130b0b0ae03a7c84e01d0d
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-20 18:24:26 +0100

    updated search-watir.rb: ARGV.join(' ')

--- lib/search_watir.rb
+++ lib/search_watir.rb
@@ -10,5 +10,5 @@ Dotenv.load
 browser = Watir::Browser.new(:firefox)
 browser.goto 'google.com'
 search_bar = browser.text_field(class: 'gsfi')
-search_bar.set(ARGV)
+search_bar.set(ARGV.join(" "))
 search_bar.send_keys(:enter)
