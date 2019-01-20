aecbc517bd71d28a3577869a2f2223b1e4927c51
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-20 18:09:03 +0100

    updated dependencies : watir and selenium-webbrowser, updated search-watir.rb: working

--- lib/search_watir.rb
+++ lib/search_watir.rb
@@ -3,5 +3,12 @@
 require 'rb-readline'
 require 'pry'
 require 'dotenv'
+require 'watir'
 
 Dotenv.load
+
+browser = Watir::Browser.new(:firefox)
+browser.goto 'google.com'
+search_bar = browser.text_field(class: 'gsfi')
+search_bar.set(ARGV)
+search_bar.send_keys(:enter)
